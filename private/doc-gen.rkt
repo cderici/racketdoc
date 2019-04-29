2343fb1bbeefcb6063f99a3dce302baea99f1bbb
Caner Derici <canerderici@gmail.com>
2019-04-29 13:03:30 -0400

    write a scrbl for each file with correct extension

--- private/doc-gen.rkt
+++ private/doc-gen.rkt
@@ -9,6 +9,13 @@
                    #:doctag doctag
                    #:block block
                    #:out-dir scribble-out)
+  (define docs-dir (build-path scribble-out "docs"))
+
+  (unless (directory-exists? docs-dir)
+    (make-directory docs-dir))
+
   (parameterize ([current-directory source-dir])
     (for ([p (in-directory)] #:when (path-has-extension? p extension))
-      (printf "- ~a\n" p))))
+      (with-output-to-file (build-path docs-dir (string-append (path->string p) ".scrbl"))
+        (lambda () (printf "- hey\n - hoy"))
+        #:exists 'replace))))
